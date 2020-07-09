4e333fc77d73f99a4d3e619a9e0f573272a727ab
Rahul-Chakravorty <58397739+rahul-lang@users.noreply.github.com>
2020-07-09 07:49:39 +0530

    Update tweak.py

--- tweak.py
+++ tweak.py
@@ -54,3 +54,22 @@ while int(accuracy) < 90 :
 	accuracy=validate(fit_model , epochs)
 	flag = 1
 
+import smtplib
+def send_email(subject, msg):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login("rchakravorty4@gmail.com, "rahulisarrow)
+        message = 'Subject: {}\n\n{}'.format(subject, msg)
+        server.sendmail(rchakravorty4@gmail.com, rchakravorty4@gmail.com, message)
+        server.quit()
+        print("Success: Email sent!")
+    except:
+        print("Email failed to send.")
+
+
+subject = "Build Message"
+msg = "Accuracy is achieved"
+
+send_email(subject, msg)
