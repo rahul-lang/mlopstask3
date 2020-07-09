bcdd35a2baf14c3b1f76068764cfb140ccbddf40
Rahul-Chakravorty <58397739+rahul-lang@users.noreply.github.com>
2020-07-09 07:47:01 +0530

    Update success.py

--- success.py
+++ success.py
@@ -1,12 +1,22 @@
 import smtplib
-s = smtplib.SMTP('smtp.gmail.com', 587)
-s.starttls()
-
-s.login("rchakravorty4@gmail.com", "rahulisarrow")
 
 
-message_success = "Achieved your desired accuracy .Congrats."
 
-s.sendmail("rchakravorty4@gmail.com", "rchakravorty4@gmail.com", message_success)
-    
-s.quit()
+def send_email(subject, msg):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login("rchakravorty4@gmail.com, "rahulisarrow)
+        message = 'Subject: {}\n\n{}'.format(subject, msg)
+        server.sendmail(rchakravorty4@gmail.com, rchakravorty4@gmail.com, message)
+        server.quit()
+        print("Success: Email sent!")
+    except:
+        print("Email failed to send.")
+
+
+subject = "Build Message"
+msg = "Accuracy is achieved"
+
+send_email(subject, msg)
