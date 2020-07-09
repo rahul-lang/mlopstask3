c62d1da89db4b956ccb38b8193b329c000a7fe3d
Rahul-Chakravorty <58397739+rahul-lang@users.noreply.github.com>
2020-07-09 07:46:10 +0530

    Update fail.py

--- fail.py
+++ fail.py
@@ -1,12 +1,22 @@
 import smtplib
-s = smtplib.SMTP('smtp.gmail.com', 587)
-s.starttls()
-
-s.login("rchakravorty4@gmail.com", "rahulisarrow")
 
 
-message = "Accuracy is less than 90%.Please try again"
 
-s.sendmail("rchakravorty4@gmail.com", "rchakravorty4@gmail.com", message)
-    
-s.quit()
+def send_email(subject, msg):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login("rchakravorty4@gmail.com, "rahulisarrow)
+        message = 'Subject: {}\n\n{}'.format(subject, msg)
+        server.sendmail(rchakravorty4@gmail.com, rchakravorty4@gmail.com, message)
+        server.quit()
+        print("Success: Email sent!")
+    except:
+        print("Email failed to send.")
+
+
+subject = "Build Message"
+msg = "Accuracy is lessa than 90%.pLease rebuild"
+
+send_email(subject, msg)
